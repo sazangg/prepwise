0b14c990fd54696da01509760f396ab59153e2b8
SAZAN <younes.lesfer@gmail.com>
2025-03-22 03:13:12 +0000

    implement interview generation

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -13,7 +13,7 @@ export default function Agent({ userName }: AgentProps) {
   const isSpeaking = true;
   const messages = [
     "What's your name?",
-    "My name is John Doe, nice to meet you",
+    "My name is John Doe, nice to meet you!",
   ];
   const lastMessage = messages[messages.length - 1];
 

b006a61ea694efc91113c459bd6f803bcdfed28d
SAZAN <younes.lesfer@gmail.com>
2025-03-24 23:48:07 +0000

    update prompt for generating questions

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -17,7 +17,7 @@ export async function POST(request: Request) {
         The job role is ${role}.
         The job experience level is ${level}.
         The tech stack used in the job is: ${techstack}.
-        The focus between behavioural and technical questions should lean towards: ${type}.
+        The focus between behavioral and technical questions should lean towards: ${type}.
         The amount of questions required is: ${amount}.
         Please return only the questions, without any additional text.
         The questions are going to be read by a voice assistant so do not use "/" or "*" or any other special characters which might break the voice assistant.
